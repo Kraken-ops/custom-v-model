19fd83e41140eed6b735ca27c75d8b6aa514c1cc
Priyanshu Dey <priyanshu.dey@globalids.com>
2023-09-19 19:47:07 +0530

    added files

---
+++ src/components/ParentComponent.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="text-center">
+    <v-dialog v-model="dialog" width="500">
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn color="blue darken-2" dark v-bind="attrs" v-on="on">
+          Click Me
+        </v-btn>
+      </template>
+
+      <v-card>
+        <span>
+          <v-card-title class="pl-8 text-h5 blue lighten-4" >
+            Privacy Policy
+            <v-icon large color="blue darken-2" id="icon" @click="dialog = false">mdi-close</v-icon>
+          </v-card-title>
+          
+        </span>
+
+        <v-card-text>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="primary" text @click="dialog = false"> I accept </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+<script>
+export default {
+    props: {
+        value: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    computed: {
+        dialog: {
+        get: function () {
+          return this.value;
+        },
+        set: function (newValue) {
+          this.$emit("input", newValue);
+        },
+      },
+    },
+  data() {
+    return {
+      
+    };
+  },
+};
+</script>
+
+<style>
+#icon{
+    margin-bottom: -8px;
+    margin-left: 250px;
+}
+</style>
